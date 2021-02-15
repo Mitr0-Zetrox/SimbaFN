23cc03eb8bb86456e56eb6058ec0acb5b2e6b8cc
Mitr0-Zetrox <77738636+Mitr0-Zetrox@users.noreply.github.com>
2021-02-15 15:21:31 +0200

    Add files via upload
    A new Private Server
    Contributors: simba#9999 and DRQSuperior#8047

---
+++ Simba Launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+Title SimbaFN Hybrid Version Credits To Simbus#5849 And simba#9999
+echo Dll Found Native
+echo SimbaFN By Simbus#5849 And simba#9999
+cd %CD%\backend
+START  launcher\FortniteLauncher.exe
+START launcher\SimbaEngine.bat
+START launcher\WarEngine.vbs
+START https://discord.gg/zsGFNzS7sx
+pause>nul
